071665fa72a2ad932be719bdd3586b92e6afba48
jampanman-git <kosukengineer@gmail.com>
2020-08-01 19:06:19 +0900

    ordersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_154823) do
+ActiveRecord::Schema.define(version: 2020_08_01_095552) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_154823) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "transactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
