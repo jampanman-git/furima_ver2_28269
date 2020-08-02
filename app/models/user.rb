b53610eaac5ef0a704c58fd7e670295e134aa3ec
jampanman-git <kosukengineer@gmail.com>
2020-08-02 13:27:24 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, inclusion: { in: "@"}
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
