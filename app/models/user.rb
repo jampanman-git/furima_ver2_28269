7be916f2f83b361392d8cd7bc07f1af87bc0deb2
jampanman-git <kosukengineer@gmail.com>
2020-08-03 19:12:12 +0900

    変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, first_name_kana: "は全角カタカナで入力して下さい。"}
   validates :birthday, presence: true
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, inclusion: { in: "@"}
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
