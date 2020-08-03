3d6a3e9a7b63406cac8fdccb1f036e2fb895e62e
jampanman-git <kosukengineer@gmail.com>
2020-08-03 11:40:50 +0900

    本人確認情報のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, last_name: "は全角で入力してください。"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, first_name: "は全角で入力してください。"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, last_name_kana: "は全角カタカナで入力して下さい。"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, first_name_kana: "は全角カタカナで入力して下さい。"}
+  validates :birthday, presence: true
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, inclusion: { in: "@"}
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
