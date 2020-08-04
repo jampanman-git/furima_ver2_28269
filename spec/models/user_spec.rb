63887cd85003288e45ccf93ffda8eac4638bf59d
jampanman-git <kosukengineer@gmail.com>
2020-08-04 16:06:26 +0900

    「新規登録がうまくいくとき」のit記載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,23 @@ describe User do
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+
       end
-      it "nicknameが6文字以下で登録できる" do
+      it "emailに一意性があれば登録できる" do
+      end
+      it "emailに@を含んでいれば登録できる"
       end
       it "passwordが6文字以上であれば登録できる" do
         @user.nickname = "aaaaaa"
         expect(@user).to be_valid
+      end
+      it "passwordが半角英数字混合であれば登録できる"
+      end
+      it "last_nameとfirst_name、last_name_kanaとfirst_name_kana、birthdayがあれば登録できる"
+      end
+      it "last_nameとfirst_nameが全角（漢字・ひらがな・カタカナ）で入力してあれば登録できる"
+      end
+      it "last_name_kanaとfirst_name_kanaが全角（カタカナ）であれば登録できる"
       end
     end
 
