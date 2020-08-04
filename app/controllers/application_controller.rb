a92f3c1db3551d19ca3967369a4e4804ce1ba1fd
jampanman-git <kosukengineer@gmail.com>
2020-08-04 11:50:54 +0900

    authenticateの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate,except: [:index,:show]
-  before_action :authenticate_user!
+  before_action :authenticate_user!,except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
