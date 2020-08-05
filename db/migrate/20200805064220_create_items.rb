6eccbffecf2218c18ad2d5d3b655fa1937222bfd
jampanman-git <kosukengineer@gmail.com>
2020-08-05 17:59:49 +0900

    アクティブハッシュのカラム除去

--- db/migrate/20200805064220_create_items.rb
+++ db/migrate/20200805064220_create_items.rb
@@ -4,11 +4,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :image, null:false
       t.string :name, null:false
       t.text :description, null:false
-      t.string :category, null:false
-      t.string :status, null:false
-      t.string :deli_fee, null:false
-      t.string :area, null:false
-      t.string :deli_day, null:false
       t.integer :price, null:false
       t.references :user, null: false, forign_key: true
       t.timestamps
