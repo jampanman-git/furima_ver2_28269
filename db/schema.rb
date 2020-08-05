d6c2618b989ef242225268facadebd12de20d68e
jampanman-git <kosukengineer@gmail.com>
2020-08-05 16:50:28 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_074221) do
+ActiveRecord::Schema.define(version: 2020_08_05_064220) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "image", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "category", null: false
+    t.string "status", null: false
+    t.string "deli_fee", null: false
+    t.string "area", null: false
+    t.string "deli_day", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
