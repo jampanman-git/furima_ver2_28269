39028866ac98b665f8217c5cb83ca2029d86e2f5
jampanman-git <kosukengineer@gmail.com>
2020-08-05 16:47:27 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category
+    validates :status
+    validates :deli_fee
+    validates :area
+    validates :deli_day
+    validates :price
+    validates :user
+  end
 end
