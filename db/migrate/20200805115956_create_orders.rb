fa9fc24ebe2e2e2ad45ad5cc3c4568026f7cc3b4
jampanman-git <kosukengineer@gmail.com>
2020-08-06 10:48:26 +0900

    ordersテーブル references修正

--- db/migrate/20200805115956_create_orders.rb
+++ db/migrate/20200805115956_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.item :item, null: false
-      t.user :user, null: false
+      t.references :item, null: false
+      t.references :user, null: false
       t.timestamps
     end
   end
