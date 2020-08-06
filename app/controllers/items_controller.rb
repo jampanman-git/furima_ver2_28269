42b5f7931bc0ac67695f65c831f4e11405a1a13f
jampanman-git <kosukengineer@gmail.com>
2020-08-06 18:51:21 +0900

    create&newメソッドの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order("created_at DESC")
   end
+
+  def new
+  end
+
+  def create
+    Item.create(image: params[:image], name: params[:name], description: params[:description], user_id: current_user.id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description,:price).merge(user_id: current_user.id)
+  end
 end
