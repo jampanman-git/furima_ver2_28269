08bf865b29bdf6dd7c659f932ca875e4d1f125fa
jampanman-git <kosukengineer@gmail.com>
2020-08-10 11:01:14 +0900

    ファクトリーボット修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,11 +4,5 @@ FactoryBot.define do
     name {"あああ"}
     description{"あああ"}
     price{"300"}
-    user_id{1}
-    category_id{2}
-    status_id{2}
-    area_id{2}
-    deli_day_id{2}
-    deli_fee_id{2}
   end
 end
