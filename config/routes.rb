e435d48c80626c803d27e7daaf0c589d17715821
jampanman-git <kosukengineer@gmail.com>
2020-08-11 00:47:40 +0900

    Revert "ルーティングshow追加"
    This reverts commit 18c6e2f304a0a8d30e77f6496a855d94148718b8.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
     
     
-  resources :items, only: [:index, :new, :create,:show] do
+  resources :items, only: [:index, :new, :create] do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
