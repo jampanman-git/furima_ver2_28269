521e9c338c4691bc1948546bc591d3c560447b3a
jampanman-git <kosukengineer@gmail.com>
2020-08-12 16:44:42 +0900

    addressesテーブル 修正

--- db/migrate/20200812065232_create_addresses.rb
+++ db/migrate/20200812065232_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :postal, null:false
-      t.integer :prefecture, null:false
+      t.integer :prefecture_id, null:false
       t.string :city, null:false
       t.string :house_num, null:false
       t.string :building
