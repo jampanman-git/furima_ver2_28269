14f2b38ab3913b25367af5c6643a3a79dc134c7f
jampanman-git <kosukengineer@gmail.com>
2020-08-12 19:32:56 +0900

    コントローラーの内容実装（仮）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @order = OrderAddress.new(order_params)
+
+   if @order.valid?
+     @order.save  # バリデーションをクリアした時
+     return redirect_to root_path
+   else
+     render "index"    # バリデーションに弾かれた時
+   end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal,:prefecture_id,:city,:house_num,:building,:phone).merge(user_id: current_user.id, item_id: item.id, order_id: order.id)
+  end
 end
