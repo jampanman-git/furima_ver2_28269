1e50d749f8159ba921f7a90d0dc656438f94238b
jampanman-git <kosukengineer@gmail.com>
2020-08-12 21:53:35 +0900

    コントローラーとビューの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,31 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = Order.all
   end
 
   def create
     @order = OrderAddress.new(order_params)
-
    if @order.valid?
      @order.save  # バリデーションをクリアした時
      return redirect_to root_path
    else
-     render "index"    # バリデーションに弾かれた時
+     render :index    # バリデーションに弾かれた時
    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:postal,:prefecture_id,:city,:house_num,:building,:phone).merge(user_id: current_user.id, item_id: item.id, order_id: order.id)
+    params.require(:order).permit(:postal,:prefecture_id,:city,:house_num,:building,:phone).merge(user_id: current_user.id)
   end
+
+  # def pay_item
+  #   Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+  #   Payjp::Charge.create(
+  #     amount: order_params[:price],  # 商品の値段
+  #     card: order_params[:token],    # カードトークン
+  #     currency:'jpy'                 # 通貨の種類
+  #   )
+  # end
 end
