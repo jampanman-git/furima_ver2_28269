7de23fc304ad684baf744cd2c395b57f5d25ecf5
jampanman-git <kosukengineer@gmail.com>
2020-08-12 16:19:39 +0900

    ordersテーブル カラム設定

--- db/migrate/20200812065232_create_addresses.rb
+++ db/migrate/20200812065232_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal, null:false
+      t.integer :prefecture, null:false
+      t.string :city, null:false
+      t.string :house_num, null:false
+      t.string :building
+      t.string :phone, null:false
+      t.references :item, null:false
       t.timestamps
     end
   end
