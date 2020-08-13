7a8b9cb333a46f5cba124575f82d6048f119943e
jampanman-git <kosukengineer@gmail.com>
2020-08-13 21:46:57 +0900

    pay_itemアクション実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,12 +24,12 @@ class OrdersController < ApplicationController
     params.permit(:postal,:area_id,:city,:house_num,:building,:phone).merge(user_id: current_user.id)
   end
 
-  # def pay_item
-  #   Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
-  #   Payjp::Charge.create(
-  #     amount: order_params[:price],  # 商品の値段
-  #     card: order_params[:token],    # カードトークン
-  #     currency:'jpy'                 # 通貨の種類
-  #   )
-  # end
+  def pay_item
+    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類
+    )
+  end
 end
