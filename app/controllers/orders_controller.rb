36f51c1e91bdd42f2bcf2584d2602ea243f0c2ca
jampanman-git <kosukengineer@gmail.com>
2020-08-13 21:48:58 +0900

    pay.jp テスト公開鍵

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_XXXXXXXX"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = "pk_test_2ced4f4aced4d3455eb3eac6"  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: order_params[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
