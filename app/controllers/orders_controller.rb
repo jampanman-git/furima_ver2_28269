fa48e31211888c466d60cbee2ce2684367e68d3e
jampanman-git <kosukengineer@gmail.com>
2020-08-14 14:45:56 +0900

    card情報実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
     @order = OrderAddress.new(order_params)
    if @order.valid?
      @order.save  # バリデーションをクリアした時
+     pay_item
      return redirect_to root_path
    else
      render :new    # バリデーションに弾かれた時
@@ -22,20 +23,21 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:postal,:area_id,:city,:house_num,:building,:phone,:token,:price).merge(user_id: current_user.id)
+    params.permit(:postal,:area_id,:city,:house_num,:building,:phone,:token).merge(user_id: current_user.id)
   end
 
   def pay_item
+    @item = Item.find(params[:item_id])
     Payjp.api_key = "pk_test_2ced4f4aced4d3455eb3eac6"  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類
     )
   end
 
   # def not_user
-  #   if current_user.id == @item.user
+  #   if current_user.id == @item.user_id
   #     redirect_to item_path(@item.id)
   #   end
   # end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -5,16 +5,18 @@ const pay = () => {
     e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
-    console.log(formResult)
     const formData = new FormData(formResult);
 
 
+
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
+    console.log(card)
+
 
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
