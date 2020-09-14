d5715fa47681bc4246b2e83c137422850898d57b
jampanman-git <kosukengineer@gmail.com>
2020-09-14 17:18:57 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   has_many :orders
 
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, last_name: "は全角で入力してください。"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, first_name: "は全角で入力してください。"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, last_name_kana: "は全角カタカナで入力して下さい。"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, first_name_kana: "は全角カタカナで入力して下さい。"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"last_nameは全角で入力してください。"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "first_nameは全角で入力してください。"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "last_name_kanaは全角カタカナで入力して下さい。"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "first_name_kanaは全角カタカナで入力して下さい。"}
     validates :birthday
     validates :nickname 
     validates :email, uniqueness: true
-    validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
+    validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, message: "passwordは半角英数字で入力してください"}
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
