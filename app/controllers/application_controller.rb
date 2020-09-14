672a6df6f3a9315df39a539db01734d9918287d0
jampanman-git <kosukengineer@gmail.com>
2020-09-14 17:16:29 +0900

    不要なコメントアウト削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
+    root_path
   end
 
   private
